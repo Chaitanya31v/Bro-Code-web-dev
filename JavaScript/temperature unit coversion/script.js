73cd2053ecaf7a376ec729dc5f588c38145f5b30
chaitanya31v <chaitanyaverma31v@gamil.com>
2025-12-30 04:37:55 +0530

    L productivity

--- JavaScript/temperature unit coversion/script.js
+++ JavaScript/temperature unit coversion/script.js
@@ -1,3 +1,26 @@
-function convert(){
-    
+// Temperature conversion program
+
+const textbox = document.getElementById("textbox");
+const tofahrenheit = document.getElementById("tofahrenheit");
+const tocelsius = document.getElementById("tocelsius");
+const result = document.getElementById("result");
+let temp;
+
+function convert() {
+
+    if (tofahrenheit.checked) {
+        temp = textbox.value;
+        temp = Number(temp);
+        temp = temp * 9/5 + 32;
+        result.textContent = temp.toFixed(1) + "°F";
+    }
+    else if (tocelsius.checked) {
+        temp = textbox.value;
+        temp = Number(temp);
+        temp = temp - 32 * 5/9;
+        result.textContent = temp.toFixed(1) + "°C";
+    }
+    else {
+        result.textContent = "select a unit";
+    }
 }
